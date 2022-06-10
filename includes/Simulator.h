03cd81bac25e2215b92348b8c21fc21a70c64dea
Liam-Lin0107 <geffc1454@gmail.com>
2022-06-10 15:30:32 +0800

    建立simulator基本架構

--- includes/Simulator.h
+++ includes/Simulator.h
@@ -11,9 +11,28 @@ using array3d = vector<vector<vector<double>>>;
 class Simulator {
 private:
     array3d price;
-
+    double S, K, q, T, sigma, Rep, Sim, TradeDays;
 
 public:
+    Simulator(double S, double K, double q, double T, double sigma, double Rep, double Sim, double TradeDays);
+
+    const array3d &getPrice() const;
+
+    double getS() const;
+
+    double getK() const;
+
+    double getQ() const;
+
+    double getT() const;
+
+    double getSigma() const;
+
+    double getRep() const;
+
+    double getSim() const;
+
+    double getTradeDays() const;
 
 };
 
--- src/Simulator.cpp
+++ src/Simulator.cpp
@@ -5,3 +5,41 @@
 #include "../includes/Simulator.h"
 
 
+Simulator::Simulator(double S, double K, double q, double T, double sigma, double Rep, double Sim, double TradeDays)
+: S(S), K(K), q(q), T(T), sigma(sigma), Rep(Rep), Sim(Sim), TradeDays(TradeDays){}
+
+const array3d &Simulator::getPrice() const {
+    return price;
+}
+
+double Simulator::getS() const {
+    return S;
+}
+
+double Simulator::getK() const {
+    return K;
+}
+
+double Simulator::getQ() const {
+    return q;
+}
+
+double Simulator::getT() const {
+    return T;
+}
+
+double Simulator::getSigma() const {
+    return sigma;
+}
+
+double Simulator::getRep() const {
+    return Rep;
+}
+
+double Simulator::getSim() const {
+    return Sim;
+}
+
+double Simulator::getTradeDays() const {
+    return TradeDays;
+}
